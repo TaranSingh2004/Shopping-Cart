6443a7eb4484acdcc4a60e9fc72473c0d6344cc3
TaranSingh2004 <taranduggal2004@gmail.com>
2025-02-21 19:48:08 +0530

    edit product functionality added in view products under admin

--- src/main/java/com/ecom/Shopping_Cart/controller/AdminController.java
+++ src/main/java/com/ecom/Shopping_Cart/controller/AdminController.java
@@ -185,4 +185,22 @@ public class AdminController {
         return "redirect:/admin/products";
     }
 
+    @GetMapping("/editProduct/{id}")
+    public String editProduct(@PathVariable int id,Model m){
+        m.addAttribute("product", productService.getProductById(id));
+        m.addAttribute("categories", categoryService.getAllCategory());
+        return "admin/edit_product";
+    }
+
+    @PostMapping("/updateProduct")
+    public String updateProduct(@ModelAttribute Product product,@RequestParam("file") MultipartFile image, HttpSession session, Model m){
+        Product updateProduct = productService.updateProduct(product, image);
+        if(!ObjectUtils.isEmpty(updateProduct)){
+            session.setAttribute("succMsg", "product updated succesfully");
+        } else {
+            session.setAttribute("errorMsg", "something wrong on server");
+        }
+        return "redirect:/admin/editProduct/" + product.getId();
+    }
+
 }
--- src/main/java/com/ecom/Shopping_Cart/services/impl/ProductServiceImpl.java
+++ src/main/java/com/ecom/Shopping_Cart/services/impl/ProductServiceImpl.java
@@ -3,11 +3,21 @@ package com.ecom.Shopping_Cart.services.impl;
 import com.ecom.Shopping_Cart.model.Product;
 import com.ecom.Shopping_Cart.repository.ProductRepository;
 import com.ecom.Shopping_Cart.services.ProductService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -34,5 +44,45 @@ public class ProductServiceImpl implements ProductService {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public Product getProductById(int id) {
+        Product product = productRepository.findById(id).orElse(null);
+        return product;
+    }
+
+    @Override
+    public Product updateProduct(Product product, MultipartFile image) {
+        Product dbProduct = getProductById(product.getId());
+        String imageName = image.isEmpty() ? dbProduct.getImage() : image.getOriginalFilename();
+        dbProduct.setTitle(product.getTitle());
+        dbProduct.setDescription(product.getDescription());
+        dbProduct.setCategory(product.getCategory());
+        dbProduct.setPrice(product.getPrice());
+        dbProduct.setStock(product.getStock());
+        dbProduct.setImage(imageName);
+
+        Product updateProduct = productRepository.save(dbProduct);
+        if (!ObjectUtils.isEmpty(updateProduct)) {
+
+            if (!image.isEmpty()) {
+                try {
+                    File saveFile = new ClassPathResource("static/img").getFile();
+
+                    Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "category_img" + File.separator
+                            + image.getOriginalFilename());
+
+                    // System.out.println(path);
+                    Files.copy(image.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+            return product;
+        }
+
+        return null;
     }
 }
